1310091feb4d5ff606ee593dbb7c13893915599d
Tonmoy-Rahman_turjo <tonmoy9828@gmail.com>
2024-01-23 03:48:56 +0600

    nav

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        transformOrigin: {
+            'top-right-1/1-3/1': '73% 7%',
+          }
+      },
+    },
+    plugins: [],
+  }
